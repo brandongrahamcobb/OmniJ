bf93e6f9b9d8c22fb383d94317f81b28998e38da
Brandon G. Cobb <brandongrahamcobb@icloud.com>
2025-06-17 13:40:22 -0400

    321st

--- src/main/java/com/brandongcobb/vyrtuous/objects/ContextEntry.java
+++ src/main/java/com/brandongcobb/vyrtuous/objects/ContextEntry.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package com.brandongcobb.vyrtuous.utils.inc;
+package com.brandongcobb.vyrtuous.objects;
 
 public class ContextEntry {
     
